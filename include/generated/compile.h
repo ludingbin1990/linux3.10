2b168b4612cb5c7d32f1dde9c28e3a246dabb174
ludingbin <ludingbin@yeah.net>
2019-09-12 06:51:18 -0700

    add the linux-3.10 code

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 89 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#89 Mon Sep 2 04:31:04 PDT 2019"
+#define LINUX_COMPILE_BY "lu"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.3.3 (Sourcery G++ Lite 2009q1-203) "
