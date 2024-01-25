2f51c50b92e135e215bc0c9936f23462dcaff2eb
dingbin.lu <dingbin.lu@thunderxauto.com>
2024-01-25 15:35:46 +0800

    fix ohci 2440 send the setup packet without the data partion

--- drivers/usb/host/ohci-s3c2410.c
+++ drivers/usb/host/ohci-s3c2410.c
@@ -435,7 +435,7 @@ static const struct hc_driver ohci_s3c2410_hc_driver = {
 	 * generic hardware linkage
 	 */
 	.irq =			ohci_irq,
-	.flags =		HCD_USB11 | HCD_MEMORY,
+	.flags =		HCD_USB11 | HCD_MEMORY | HCD_LOCAL_MEM,
 
 	/*
 	 * basic lifecycle operations
